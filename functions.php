dcc98f00154bdd6f15766977ea94cf77078fc049
Patrick Boettcher <patrick.boettcher@posteo.de>
2014-12-13 13:07:18 +0100

    import upstream 1.2.1

--- functions.php
+++ functions.php
@@ -49,7 +49,8 @@ add_action( 'after_setup_theme', 'skt_full_width_setup' );
 function skt_full_width_widgets_init() {
 
 	register_sidebar( array(
-		'name'          => __( 'Primary Sidebar', 'skt-full-width' ),
+		'name'          => __( 'Blog Sidebar', 'skt-full-width' ),
+		'description'   => __( 'Appears on blog page sidebar', 'skt-full-width' ),
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
--- sidebar.php
+++ sidebar.php
@@ -6,27 +6,7 @@
  */
 ?>
 <div id="sidebar">
-    <div class="recent-post">
-    	<h1 class="widget-title">Recent Posts</h1>
-		<?php $the_query = new WP_Query( 'posts_per_page=2' ); ?>
-        <?php if( $the_query->have_posts() ) : while( $the_query->have_posts() ) : $the_query->the_post(); ?>
-            <div class="post-box">
-                <div class="thumb"><?php the_post_thumbnail(); ?></div><!-- thumb -->
-                <div class="post-text">
-					<?php
-                    $excerpt = get_the_excerpt();
-                    echo skt_full_width_string_limit_words($excerpt,7);
-                    ?>
-                    <div><a href="<?php the_permalink(); ?>">Read more..</a></div>
-                </div><!-- post-text -->
-            </div><!-- post-box -->
-            <div class="clear"></div>
-        <?php endwhile; else : endif; ?>
-        <?php wp_reset_postdata(); ?>
-    </div><!-- recent-post -->
     
-	<?php do_action( 'before_sidebar' ); ?>
-
     <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
         <aside id="archives" class="widget">
             <h1 class="widget-title"><?php _e( 'Archives', 'skt-full-width' ); ?></h1>
